46e75ce21357ffd0034eacb4ee2b086d8aa41c24
twallema <tallema2@jh.edu>
2026-04-13 17:32:36 -0400

    data folder

---
+++ data/conversion/demography/build-demography.py
@@ -0,0 +1,64 @@
+"""
+A script to build a list containing US state and county names and corresponding FIPS codes
+"""
+
+############################
+## Load required packages ##
+############################
+
+import os
+import pandas as pd
+
+year = 2023
+
+#######################
+## Format FIPS codes ##
+#######################
+
+# load FIPS codes & slice relevant columns
+state_FIPS = pd.read_csv(os.path.join(os.getcwd(), '../../raw/demography/national_state2020.txt'), delimiter='|')[['STATE', 'STATEFP', 'STATE_NAME']]
+# the following states/territories are removed from the demographic data
+remove_state_FIPS = [60, 66, 69, 74, 78]
+# remove undesired states and territories
+state_FIPS = state_FIPS[~state_FIPS['STATEFP'].isin(remove_state_FIPS)]
+# rename columns
+state_FIPS = state_FIPS.rename(columns={"STATE": "abbreviation_state","STATEFP": "fips_state", "STATE_NAME": "name_state",})
+# use lowercase only 
+state_FIPS['name_state'] = state_FIPS['name_state'].apply(lambda x: x.lower())
+
+################################
+## Load and format demograhpy ##
+################################
+
+# load demographic data
+state_demo = pd.read_csv(os.path.join(os.getcwd(), '../../raw/demography/sc-est2023-agesex-civ.csv'))
+# select right sex and year
+state_demo = state_demo[((state_demo['SEX'] == 0) & (state_demo['AGE'] != 999))][['DIVISION','STATE', 'AGE', f'POPEST{year}_CIV']]
+# aggregate data to total population (2023)
+agg = state_demo.groupby(['DIVISION','STATE'], observed=False)[f'POPEST{year}_CIV'].sum().reset_index()
+# attach region names
+region_mapping = ['United States Total', 'New England', 'Middle Atlantic', 'East North Central', 'West North Central', 'South Atlantic', 'East South Central', 'West South Central', 'Mountain', 'Pacific']
+# map REGION values to their corresponding names
+agg['region_name'] = agg['DIVISION'].map(lambda x: region_mapping[x])
+
+###########################
+## Merge and save result ##
+###########################
+
+# merge the dataframes on the corresponding columns
+merged_df = state_FIPS.merge(agg, left_on="fips_state", right_on="STATE", how="left")
+# drop the redundant STATE column after merging
+out = merged_df.drop(columns=["STATE", "DIVISION"])
+# rename column
+out = out.rename(columns={f"POPEST{year}_CIV": "population"})
+
+# add puerto rico 2023 demography manually
+out.loc[out['name_state'] == 'puerto rico', 'population'] = 3.204E6
+out.loc[out['name_state'] == 'puerto rico', 'region_name'] = 'South Atlantic'
+
+# add the US as a "state"
+out.loc[len(out)] = ['USA', 0, 'united states', sum(out['population']),'NA']
+out = out.sort_values(by='fips_state')
+
+# save
+out.to_csv(os.path.join(os.getcwd(),'../../interim/demography/demography.csv'), index=False)
